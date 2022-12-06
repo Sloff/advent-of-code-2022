d68c695b4969acc58d4cb5d83e3af1725502ce59
Ettienne Pitts <ettpitts@pm.me>
2022-12-06 08:36:00 +0200

    feat: Cleanup

--- cmd/day2.go
+++ cmd/day2.go
@@ -42,14 +42,4 @@ var day2Cmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(day2Cmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// day1Cmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// day1Cmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
